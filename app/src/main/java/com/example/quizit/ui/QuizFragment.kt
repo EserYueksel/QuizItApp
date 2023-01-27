29b729f3a302294f45b49378ada8530aa562a221
EserYueksel <eseryuksel@live.de>
2023-01-27 13:58:00 +0100

    -LiveData added

--- app/src/main/java/com/example/quizit/ui/QuizFragment.kt
+++ app/src/main/java/com/example/quizit/ui/QuizFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.quizit.QuizViewModel
 import com.example.quizit.databinding.FragmentQuizBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -15,7 +17,7 @@ class QuizFragment : Fragment() {
 
     private lateinit var binding: FragmentQuizBinding
 
-    private val viewModel: QuizViewModel by viewModels()
+    private val viewModel: QuizViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,37 +29,27 @@ class QuizFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        updateUi()
+
+        viewModel.score.observe(viewLifecycleOwner){
+            binding.scoreText.text = it.toString()
+        }
+        viewModel.currentQuestion.observe(viewLifecycleOwner) {
+            binding.questionText.text = it.name
+        }
+        viewModel.gameEnd.observe(viewLifecycleOwner) {
+            if (it) {
+                findNavController()
+                    .navigate(QuizFragmentDirections.actionQuizFragmentToResultFragment())
+            }
+        }
+
         binding.musicianButton.setOnClickListener {
             viewModel.checkAnswer(true)
 
-            updateUi()
         }
         binding.footballButton.setOnClickListener {
             viewModel.checkAnswer(false)
 
-            updateUi()
         }
-    }
-    private fun updateUi() {
-        binding.scoreText.text = viewModel.score.toString()
-        binding.questionText.text = viewModel.currentQuestion.name
-
-        if (viewModel.score == 5) {
-            showEndDialogue()
-        }
-    }
-    private fun showEndDialogue() {
-        MaterialAlertDialogBuilder(requireContext()).setTitle("Gut gemacht!")
-            .setMessage("Nicht schlecht!")
-            .setCancelable(false)
-            .setNegativeButton("Exit") {_ , _ ->
-                activity?.finish()
-            }
-            .setPositiveButton("AGAIN") { _ , _ ->
-                viewModel.restartGame()
-                updateUi()
-            }
-            .show()
     }
 }
